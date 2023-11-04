4e6c5420f6a2cc9e033ff8684ee66e5abe9b04e0
marisagirotto <126510606+marisagirotto@users.noreply.github.com>
2023-11-03 22:54:33 -0300

    Add files via upload

---
+++ Rádio gremio estudaantil/main.js
@@ -0,0 +1,47 @@
+function tocaSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+document.querySelector('.tecla_aplausos').onclick = tocaSomAplausos;
+
+function tocaSomVaia(){
+    document.querySelector('#som_tecla_vaia').play();
+}
+document.querySelector('.tecla_vaia').onclick = tocaSomVaia;
+
+function tocaSomRisada(){
+    document.querySelector('#som_tecla_risada').play();
+}
+document.querySelector('.tecla_risada').onclick = tocaSomRisada;
+
+function tocaSomErrado(){
+    document.querySelector('#som_tecla_errado').play();
+}
+document.querySelector('.tecla_errado').onclick = tocaSomErrado;
+
+function tocaSomGrilo(){
+    document.querySelector('#som_tecla_grilo').play();
+}
+document.querySelector('.tecla_grilo').onclick = tocaSomGrilo;
+
+function tocaSomTambores(){
+    document.querySelector('#som_tecla_tambores').play();
+}
+document.querySelector('.tecla_tambores').onclick = tocaSomTambores;
+
+function tocaSomTrombeta(){
+    document.querySelector('#som_tecla_trombeta').play();
+}
+document.querySelector('.tecla_trombeta').onclick = tocaSomTrombeta;
+
+function tocaSomDinheiro(){
+    document.querySelector('#som_tecla_dinheiro').play();
+}
+document.querySelector('.tecla_dinheiro').onclick = tocaSomDinheiro;
+
+function tocaSomVitoria(){
+    document.querySelector('#som_tecla_vitoria').play();
+}
+document.querySelector('.tecla_vitoria').onclick = tocaSomVitoria;
+
+
+
